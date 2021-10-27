d787f19b52efe5407cf4d222a0d604883cd44a20
bindi-v <bindi.vaghela@gmail.com>
2021-10-27 13:44:33 -0400

    added api/routes for post and user

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
-const blogPostRoutes = require('./blog-post-routes.js');
+const blogPostRoutes = require('./blog-route.js');
 
 router.use('/', homeRoutes);
 router.use('/blog-post', blogPostRoutes);

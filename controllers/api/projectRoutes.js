b7fa62eb77aca8c39c799f74b4d04a013bbaf505
bindi-v <bindi.vaghela@gmail.com>
2021-10-28 11:58:21 -0400

    created pod, project and selfcare routes

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+const { Project } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const newProject = await Project.findAll({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.get('/projects', async (req, res) => {
+    res.render('allprojectposts');
+  });
+
+  router.get('/:id', withAuth, async (req, res) => {
+    const body = req.body;
+  
+    try {
+      const newProject = await Project.findByPk({ ...body, userId: req.session.userId });
+      res.json(newProject);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newProject = await Project.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Get a project
+router.get('/projects/:id', async (req, res) => {
+    // This method renders the 'project' template, and uses params to select the correct project to render in the template, based on the id of the project.
+    // Now, we have access to a project description in the 'project' template.
+    return res.render('allprojectposts', project[req.params.num - 1]);
+  });
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const projectData = await Project.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
-const blogPostRoutes = require('./blog-route.js');
+const blogPostRoutes = require('./blog-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/blog-post', blogPostRoutes);

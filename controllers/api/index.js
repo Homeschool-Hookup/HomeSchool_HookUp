366612f4417c7cc1bc6e5c87e5cfd3da0ac47925
Rose Luu <luuhongnhung10@gmail.com>
2021-10-28 10:13:32 -0500

    update model and seeds for each post

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-
-const userRoutes = require('./user-routes.js');
-const blogPostRoutes = require('./blog-routes');
+const userRoutes = require("./user-routes.js");
+const postRoutes = require("./post-routes.js");
 //const commentRoutes = require('./comment-routes');
 
-
-router.use('/user', userRoutes);
-router.use('/blog-post', postRoutes);
+router.use("/user", userRoutes);
+router.use("/blog-post", postRoutes);
 //router.use('/comment', commentRoutes);
 
 module.exports = router;

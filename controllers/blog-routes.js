46898f0e7f6e3fe790d3a9473babdca4ee935c5e
bindi-v <bindi.vaghela@gmail.com>
2021-10-29 12:14:44 -0400

    fixing on post routes to work properly

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Post } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   const body = req.body;
   try {
     const postData = await Post.findAll({

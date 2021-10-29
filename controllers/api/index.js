08f3498b9bf607df69c9472288bc6e30471bb3c3
bindi-v <bindi.vaghela@gmail.com>
2021-10-28 22:59:14 -0400

    created profile Routes in api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,11 +6,13 @@ const userRoutes = require('./user-routes.js');
 const podRoutes = require('./podRoutes');
 const projectRoutes = require('./projectRoutes');
 const selfCareRoutes = require('./selfCareRoutes');
+const profileRoutes = require('./profileRoutes.js');
 
 router.use('/user', userRoutes);
 router.use('/pod', podRoutes);
 router.use('/projects', projectRoutes);
 router.use('/self-care', selfCareRoutes);
+router.use('/profile', profileRoutes);
 //router.use('/blog-post', postRoutes);
 //router.use('/comment', commentRoutes);
 
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,42 @@
 const router = require('express').Router();
+//const Profile = require('../models/Profile');
 
 router.get('/', (req, res) => {
   res.render('homepage');
 });
 
+// route to get all blog-posts
+// router.get('/post', async (req, res) => {
+//   const postData = await Post.findAll().catch((err) => { 
+//       res.json(err);
+//     });
+//       const posts = postData.map((dish) => post.get({ plain: true }));
+//       res.render('allpodpost', { posts });
+//     });
+
+    // router.get('/post', (req, res) => {
+    //   res.render('allpodpost');
+    // });
+   
+// route to get one profile
+// router.get('/profile/:id', async (req, res) => {
+//   try{ 
+//       const profileData = await Profile.findByPk(req.params.id);
+//       if(!profileData) {
+//           res.status(404).json({message: 'No profile with this id!'});
+//           return;
+//       }
+//       const profile = profileData.get({ plain: true });
+//       res.render('profile', profile);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     };     
+// });
+
 router.get('/profile', (req, res) => {
   res.render('profile');
 });
+
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {

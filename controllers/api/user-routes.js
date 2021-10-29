bfb8c02e46c3ea684cd5970d412fb0742501442e
Caitlin Wall <caitlinwall96@gmail.com>
2021-10-29 14:56:17 -0400

    Get login to work

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -46,7 +46,7 @@ router.post('/login', async (req, res) => {
         username: req.body.username,
       },
     });
-
+    
     if (!user) {
       res.status(400).json({ message: 'No user account found!' });
       return;
@@ -59,11 +59,10 @@ router.post('/login', async (req, res) => {
       return;
     }
 
-    req.session.save(() => {
+    req.session.save(() => {    
       req.session.userId = user.id;
       req.session.username = user.username;
       req.session.loggedIn = true;
-
       res.json({ user, message: 'You are now logged in!' });
     });
   } catch (err) {

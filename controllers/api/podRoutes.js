8d1365a9d49400e94f6d057b34515084665f69c5
Rose Luu <luuhongnhung10@gmail.com>
2021-10-30 01:38:30 -0500

    make pod post functional work

--- controllers/api/podRoutes.js
+++ controllers/api/podRoutes.js
@@ -3,13 +3,9 @@ const { Pod, User } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.get("/allpodpost", async (req, res) => {
-  const body = req.body;
-
   try {
     const newPod = await Pod.findAll({
       include: [User],
-      // ...req.body,
-      // user_id: req.session.user_id,
     });
     const pods = newPod.map((pod) => pod.get({ plain: true }));
     console.log("pods", pods);
@@ -17,43 +13,12 @@ router.get("/allpodpost", async (req, res) => {
       layout: "main",
       pods,
     });
-    // res.status(200).json(newPod);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// router.get('/allpodpost', async (req, res) => {
-//   res.render('allpodpost');
-// });
-
-router.get("/:id", withAuth, async (req, res) => {
-  const body = req.body;
-
-  try {
-    const newPod = await Pod.find({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newPod);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-// Get a pod
-router.get("/:id", async (req, res) => {
-  // This method renders the 'pod' template, and uses params to select the correct pod to render in the template, based on the id of the pod.
-  // Now, we have access to a pod description in the 'pod' template.
-  return res.render("allpodpost", pod[req.params.num - 1]);
-});
-// router.get("/allpodpost/new", (req, res) => {
-//   res.render("newpodpost", {
-//     layout: "main",
-//   });
-// });
-
+//render newpodpost
 router.get("/allpodpost/new", (req, res) => {
   res.render("newpodpost", {
     layout: "main",
@@ -73,14 +38,47 @@ router.post("/allpodpost/new", withAuth, async (req, res) => {
   }
 });
 
-router.put("/:id", withAuth, async (req, res) => {
+// Get single pod post, render single pod
+router.get("/allpodpost/:id", withAuth, async (req, res) => {
+  try {
+    const podData = await Pod.findByPk(req.params.id, {
+      include: [User],
+    });
+    console.log("test     ", podData);
+    const pod = podData.get({ plain: true });
+    console.log("!!!!!!!!111", pod);
+    res.render("singlepod", { pod });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//render updated page
+router.get("/allpodpost/edit/:id", withAuth, async (req, res) => {
+  try {
+    const podData = await Pod.findByPk(req.params.id, {
+      include: [User],
+    });
+    if (podData) {
+      const pod = podData.get({ plain: true });
+      console.log("!!!!!!!!*********", pod);
+      res.render("updatepod", { pod });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect("login");
+  }
+});
+router.put("/allpodpost/edit/:id", withAuth, async (req, res) => {
   try {
     const [affectedRows] = await Pod.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
-
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!", affectedRows);
+    console.log(req.body);
     if (affectedRows > 0) {
       res.status(200).end();
     } else {
@@ -91,12 +89,13 @@ router.put("/:id", withAuth, async (req, res) => {
   }
 });
 
-router.delete("/:id", withAuth, async (req, res) => {
+router.delete("/allpodpost/edit/:id", withAuth, async (req, res) => {
   try {
+    console.log("jjjjjjjjjjjjj", req.params.id);
+
     const podData = await Pod.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
       },
     });
 

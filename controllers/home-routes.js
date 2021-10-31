8518c29ed2f5c558766fb98cd944cd9ab019f5e2
Rose Luu <luuhongnhung10@gmail.com>
2021-10-31 15:06:56 -0500

    get username to show on profile page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,26 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 //const Profile = require('../models/Profile');
 
-router.get('/', (req, res) => {
-  res.render('homepage');
+router.get("/", (req, res) => {
+  res.render("homepage");
 });
 
 // route to get all blog-posts
 // router.get('/post', async (req, res) => {
-//   const postData = await Post.findAll().catch((err) => { 
+//   const postData = await Post.findAll().catch((err) => {
 //       res.json(err);
 //     });
 //       const posts = postData.map((dish) => post.get({ plain: true }));
 //       res.render('allpodpost', { posts });
 //     });
 
-    // router.get('/post', (req, res) => {
-    //   res.render('allpodpost');
-    // });
-   
+// router.get('/post', (req, res) => {
+//   res.render('allpodpost');
+// });
+
 // route to get one profile
 // router.get('/profile/:id', async (req, res) => {
-//   try{ 
+//   try{
 //       const profileData = await Profile.findByPk(req.params.id);
 //       if(!profileData) {
 //           res.status(404).json({message: 'No profile with this id!'});
@@ -30,30 +32,41 @@ router.get('/', (req, res) => {
 //       res.render('profile', profile);
 //     } catch (err) {
 //         res.status(500).json(err);
-//     };     
+//     };
 // });
 
-router.get('/profile', (req, res) => {
-  res.render('profile');
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.userId, {
+      attributes: { exclude: ["password"] },
+    });
+    const user = userData.get({ plain: true });
+    res.render("profile", {
+      ...user,
+      loggedIn: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // res.render("profile");
 });
 
-
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
-router.get('/signup', (req, res) => {
+router.get("/signup", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('signup');
+  res.render("signup");
 });
 
 module.exports = router;

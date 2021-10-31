221d4f858e58e5501e73d5d9764c9c19a2bc5c5f
Rose Luu <luuhongnhung10@gmail.com>
2021-10-31 16:05:34 -0500

    just try to get rid of all comment code and fix the signup.js to redirect user to profile page

--- models/index.js
+++ models/index.js
@@ -2,12 +2,8 @@ const Pod = require("./pod-post");
 const Project = require("./project-post");
 const SelfCarePost = require("./self-care-post");
 const User = require("./user");
-const Post = require("./post");
 
-//user has many post
-// User.hasMany(Post, {
-//   foreignKey: "user_id",
-// });
+//user can have many post
 User.hasMany(Pod, {
   foreignKey: "user_id",
 });
@@ -17,16 +13,7 @@ User.hasMany(Project, {
 User.hasMany(SelfCarePost, {
   foreignKey: "user_id",
 });
-//post will have pod, project and self-care
-// Post.hasMany(Pod, {
-//   foreignKey: "podPost_id",
-// });
-// Post.hasMany(Project, {
-//   foreignKey: "projectPost_id",
-// });
-// Post.hasMany(SelfCarePost, {
-//   foreignKey: "selfCarePost_id",
-// });
+
 //Post belong to one user
 Pod.belongsTo(User, {
   foreignKey: "user_id",
--- models/pod-post.js
+++ models/pod-post.js
@@ -33,20 +33,6 @@ Pod.init(
         key: "id",
       },
     },
-    // post_id: {
-    //   type: DataTypes.INTEGER,
-    //   references: {
-    //     model: "post",
-    //     key: "id",
-    //   },
-    // },
-    // tag_id: {
-    //   type: DataTypes.INTEGER,
-    //   references: {
-    //     model: "tag",
-    //     key: "id",
-    //   },
-    // },
   },
   {
     sequelize,
